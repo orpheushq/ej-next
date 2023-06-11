c2e6aef0989f6155db4c893917478b047c25c245
PraveenOnMac <3007446+praveenjayakody@users.noreply.github.com>
2023-06-11 15:37:42 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,10 @@ export default function Login() {
   return (
     <form>
       <div className="h-screen flex flex-col justify-center items-center space-y-2">
-        <h1 className="heading-1">Sign In</h1>
+        <h1>Sign In</h1>
         <input type="email" placeholder="Email" className="input input-bordered w-full max-w-xs" />
         <input type="password" placeholder="Password" className="input input-bordered w-full max-w-xs" />
-        <button className="btn btn-primary">Login</button>
+        <button type="submit" className="btn btn-primary">Login</button>
       </div>
     </form>
   )
